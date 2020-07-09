3b2aae4936c56e9e4aabd2bbe86ff1f3f1d64b79
Underdisc <connortdeakin@gmail.com>
2020-07-09 12:52:21 +0100

    Some minor styling changes for faster hover transitions. The display of the sidebar is almost entirely controlled within OnDropdownClick and WindowResize in main.js. The media query in style.css only controls the display of the sidebar on load. main.js takes care of the rest.

--- redesign/main.js
+++ redesign/main.js
@@ -1,15 +1,58 @@
+let dropdown_open = false;
+let compact_mode;
+
+// This comes from media queries within style.css. It is the threshold used to
+// determine the layout change.
+const compact_em_threshold = 70;
+
+const root = document.querySelector('html');
+const content = document.querySelector('div.page_content');
+const sidebar = document.querySelector('div.sidebar_grid');
+
+function GetEmWidth()
+{
+  let width = window.innerWidth;
+  let root_style = window.getComputedStyle(root);
+  let font_size = parseFloat(root_style.getPropertyValue('font-size'));
+  let em_width = width / font_size;
+  return em_width;
+}
+
 function OnDropdownClick()
 {
-  var content = document.getElementById("content");
-  var sidebar = document.getElementById("sidebar");
-  if (content.style.display != "none")
+  if (dropdown_open === false)
   {
-    content.style.display = "none";
-    sidebar.style.display = "block";
+    dropdown_open = true;
+    sidebar.style.display = 'block';
   }
   else
   {
-    content.style.display = "block";
-    sidebar.style.display = "none";
+    dropdown_open = false;
+    sidebar.style.display = 'none';
   }
 }
+
+function WindowResize()
+{
+  let em_width = GetEmWidth();
+  if (em_width <= compact_em_threshold && compact_mode === false)
+  {
+    dropdown_open = false;
+    compact_mode = true;
+    sidebar.style.display = 'none';
+  }
+  else if (em_width >= compact_em_threshold && compact_mode == true)
+  {
+    compact_mode = false;
+    sidebar.style.display = 'grid';
+  }
+}
+
+function OnLoad()
+{
+  let em_width = GetEmWidth();
+  compact_mode = em_width <= compact_em_threshold;
+}
+
+window.onresize = WindowResize;
+window.onload = OnLoad;
