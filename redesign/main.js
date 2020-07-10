5a0e77b2d36409141b92d4f42b961bb73b490156
Underdisc <connortdeakin@gmail.com>
2020-07-10 12:02:09 +0100

    The sidebar now drops down smoothly on mobile mode using a quad in ease type. In mobile mode, the sidebar is a dropdown. When the dropdown is opened, it will slide into view and the content underneath will slide at the same time to make room for the dropdown.

--- redesign/main.js
+++ redesign/main.js
@@ -1,4 +1,5 @@
 let dropdown_open = false;
+let dropdown_in_motion = false;
 let compact_mode;
 
 // This comes from media queries within style.css. It is the threshold used to
@@ -8,6 +9,7 @@ const compact_em_threshold = 70;
 const root = document.querySelector('html');
 const content = document.querySelector('div.page_content');
 const sidebar = document.querySelector('div.sidebar_grid');
+const dropdown_button = document.querySelector('div.header_dropdown_button');
 
 function GetEmWidth()
 {
@@ -18,19 +20,84 @@ function GetEmWidth()
   return em_width;
 }
 
-function OnDropdownClick()
+function GetDropdownPixelOffset()
 {
+  sidebar_style = getComputedStyle(sidebar);
+  let element_count = parseFloat(
+    sidebar_style.getPropertyValue('--element-count'));
+  let buffer_height = parseFloat(
+    sidebar_style.getPropertyValue('--buffer-height'));
+  let element_height = parseFloat(
+    sidebar_style.getPropertyValue('--element-height'));
+  let em_offset = buffer_height + (element_count * element_height);
+  let font_size = parseFloat(sidebar_style.getPropertyValue('font-size'));
+  return em_offset * font_size;
+}
+
+function ToggleDropdown()
+{
+  if(dropdown_in_motion === true)
+  {
+    return;
+  }
+
+  let dropdown_offset = GetDropdownPixelOffset();
+  let start_pos;
+  let end_pos;
   if (dropdown_open === false)
   {
+    sidebar.style.bottom = dropdown_offset + 'px';
+    sidebar.style.display = 'grid';
+    content.style.bottom = dropdown_offset + 'px';
+    start_pos = dropdown_offset;
+    end_pos = 0;
     dropdown_open = true;
-    sidebar.style.display = 'block';
   }
   else
   {
+    start_pos = 0;
+    end_pos = dropdown_offset;
     dropdown_open = false;
-    sidebar.style.display = 'none';
+  }
+  const dist = end_pos - start_pos;
+
+  // All time values are expressed in milliseconds.
+  const animation_time = 250;
+  const frame_time = 10;
+  let animation = setInterval(step, frame_time);
+  dropdown_in_motion = true;
+  const start_time = new Date();
+
+  function step()
+  {
+    let current_time = new Date();
+    let time_passed = current_time - start_time;
+    if (time_passed >= animation_time)
+    {
+      dropdown_in_motion = false;
+      if(dropdown_open === false)
+      {
+        sidebar.style.display = 'none';
+        content.style.bottom = '0px';
+      }
+      else
+      {
+        sidebar.style.bottom = end_pos + 'px';
+        content.style.bottom = end_pos + 'px';
+      }
+      clearInterval(animation);
+      return;
+    }
+
+    let t = time_passed / animation_time;
+    let t_quad_in = -1.0 * (t - 1.0) * (t - 1.0) + 1.0;
+    let current_pos = start_pos + dist * t_quad_in;
+    sidebar.style.bottom = current_pos + 'px';
+    content.style.bottom = current_pos + 'px';
   }
 }
+
+dropdown_button.addEventListener('click', ToggleDropdown);
 
 function WindowResize()
 {
@@ -45,8 +112,12 @@ function WindowResize()
   {
     compact_mode = false;
     sidebar.style.display = 'grid';
+    sidebar.style.bottom = 0;
+    content.style.bottom = 0;
   }
 }
+
+window.onresize = WindowResize;
 
 function OnLoad()
 {
@@ -54,5 +125,4 @@ function OnLoad()
   compact_mode = em_width <= compact_em_threshold;
 }
 
-window.onresize = WindowResize;
 window.onload = OnLoad;
