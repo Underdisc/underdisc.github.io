c8f19a6ef8b4b239f50afcb3fec3f40a5177d91d
Underdisc <connortdeakin@gmail.com>
2020-07-26 18:18:28 +0100

    The build script will now check the timestamps on markdown-html file pairs to see if the html file is up to date with the markdown file. If it is up to date, no rendering will be performed.

--- src/build.js
+++ src/build.js
@@ -33,17 +33,45 @@ function CorrectLink(html, selector, attribute, correction)
     linkElement.attr(attribute, filePath);
 }
 
-function RenderMarkdown(input, destination)
+function IsBuildNeeded(inputFile, outputFile)
 {
-  // todo: Before performing all the work that needs to be done to generate the
-  // html file, you should check the timestamps of the source and the output
-  // to see if an up to date file already exists.
+  exists = fs.existsSync(outputFile);
+  if(!exists)
+  {
+    return true;
+  }
+
+  let inputStats = fs.statSync(inputFile);
+  let outputStats = fs.statSync(outputFile);
+  let inputModifiedDate = inputStats.mtime;
+  let outputModifiedDate = outputStats.mtime;
+  return inputModifiedDate >= outputModifiedDate;
+}
+
+function RenderMarkdown(inputFile, destination)
+{
+  // Before rendering a markdown document to html, we first check to see if the
+  // output html document is already up to date with the current markdown
+  // document. If it is, no rendering is required.
+  let exists = fs.existsSync(inputFile);
+  if(!exists)
+  {
+    console.error(inputFile + ' does not exist');
+    return;
+  }
+  let filenameEnd = inputFile.indexOf('.');
+  let outputFile = inputFile.slice(0, filenameEnd);
+  outputFile = destination + outputFile + '.html';
+  if(!IsBuildNeeded(inputFile, outputFile))
+  {
+    return;
+  }
 
   // Before rendering the markdown document, we first fix all the links
   // contained in the template html if the output html will exist in a
   // directory below the root.
   let template = cheerio.load(templateHtml);
-  let depth = GetOccurrenceCount(input, '/');
+  let depth = GetOccurrenceCount(inputFile, '/');
   if(depth > 0)
   {
     let linkCorrection = '';
@@ -61,7 +89,7 @@ function RenderMarkdown(input, destination)
   }
 
   // Render the markdown document to html.
-  let markdown = fs.readFileSync(input, 'utf8');
+  let markdown = fs.readFileSync(inputFile, 'utf8');
   let html = converter.makeHtml(markdown);
   let content_div = '<div class=\'page_content\'></div>';
   template('div.content_container').append(content_div);
@@ -86,9 +114,6 @@ function RenderMarkdown(input, destination)
 
   // Ouput the new html to its destination.
   let outputHtml = template.html();
-  let filenameEnd = input.indexOf('.');
-  let outputFile = input.slice(0, filenameEnd);
-  outputFile = destination + outputFile + '.html';
   fs.writeFileSync(outputFile, outputHtml, 'utf8');
 }
 
