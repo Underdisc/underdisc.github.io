e5b084338a666c33cace1da4ec2712d6c8b0b5a5
Underdisc <connortdeakin@gmail.com>
2020-07-26 17:40:23 +0100

    The build process also takes care of syntax highlighting content contained with code elements now. A very basic theme is being used for the syntax highlighting in hljs_style.css. This theme only covers what is currently on the website now and will be expanded as more code snippets are added.

--- src/build.js
+++ src/build.js
@@ -23,6 +23,7 @@ let templateHtml = GetFileContent('template.html');
 const cheerio = require('cheerio');
 let showdown = require('showdown');
 let converter = new showdown.Converter();
+let hljs = require('highlight.js');
 
 function CorrectLink(html, selector, attribute, correction)
 {
@@ -38,7 +39,6 @@ function RenderMarkdown(input, destination)
   // html file, you should check the timestamps of the source and the output
   // to see if an up to date file already exists.
 
-
   // Before rendering the markdown document, we first fix all the links
   // contained in the template html if the output html will exist in a
   // directory below the root.
@@ -52,6 +52,7 @@ function RenderMarkdown(input, destination)
       linkCorrection += '../'
     }
     CorrectLink(template, 'link.main_style', 'href', linkCorrection);
+    CorrectLink(template, 'link.hljs_style', 'href', linkCorrection);
     CorrectLink(template, 'a.index_link', 'href', linkCorrection);
     CorrectLink(template, 'a.blog_link', 'href', linkCorrection);
     CorrectLink(template, 'a.portfolio_link', 'href', linkCorrection);
@@ -59,17 +60,36 @@ function RenderMarkdown(input, destination)
     CorrectLink(template, 'script.main_script', 'src', linkCorrection);
   }
 
-  // Render the markdown document to html and output the file to its
-  // destination.
+  // Render the markdown document to html.
   let markdown = fs.readFileSync(input, 'utf8');
   let html = converter.makeHtml(markdown);
   let content_div = '<div class=\'page_content\'></div>';
   template('div.content_container').append(content_div);
   template('div.page_content').append(html);
+
+  // Take all of the content within code elements where a language is defined
+  // and apply syntax highlighting to them.
+  template('code').each(function(i, domElement)
+  {
+    let fullLangString = template(this).attr('class');
+    if(typeof(fullLangString) === 'undefined')
+    {
+      return;
+    }
+
+    let langEnd = fullLangString.indexOf(' ');
+    let lang = fullLangString.slice(0, langEnd);
+    let bareContent = template(this).html();
+    let highlightedContent = hljs.highlight(lang, bareContent).value;
+    template(this).replaceWith('<code>' + highlightedContent + '</code>');
+  });
+
+  // Ouput the new html to its destination.
+  let outputHtml = template.html();
   let filenameEnd = input.indexOf('.');
   let outputFile = input.slice(0, filenameEnd);
   outputFile = destination + outputFile + '.html';
-  fs.writeFileSync(outputFile, template.html(), 'utf8');
+  fs.writeFileSync(outputFile, outputHtml, 'utf8');
 }
 
 RenderMarkdown('index.md', '../');
